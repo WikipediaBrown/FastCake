b8e7b875c5cf31a367cd03080c5d1845305b84dc
nonplus <perrisdavis@me.com>
2021-06-05 17:53:50 -0700

    Initial Commit

---
+++ FastCake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastCake
+//
+//  Created by nonplus on 6/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
